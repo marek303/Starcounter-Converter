b2aa4f0321a29db200f443d2e37be1f695c1f023
marek303 <marek_lucki@poczta.onet.pl>
2017-11-26 22:36:42 +0100

    Coding problem comments

--- 0.bat
+++ 0.bat
@@ -1,1 +1,7 @@
+#My cmake command to build local VS 2013 solution:
+#- this command should be executed from command line in folder that contains source files
+#- change DCMAKE_C_COMPILER and DCMAKE_CXX_COMPILER to appropriate paths (if needed)
+#- i did not found a solution for define parameters DCMAKE_C_COMPILER and DCMAKE_CXX_COMPILER inside CMakeLists. 
+#  Because there is a text coding problem with polish letter 'ó' in my compiler's path.
+
 cmake -DCMAKE_C_COMPILER="D:/Pliki programów (x86)/Microsoft Visual Studio 12.0/VC/bin/cl.exe" -DCMAKE_CXX_COMPILER="D:/Pliki programów (x86)/Microsoft Visual Studio 12.0/VC/bin/cl.exe"
